b4f9def6ae158b5ada881668a75161d96da5510b
Ashkan Rahmani <ashkan.rmk@hotmail.com>
2018-10-26 20:10:51 +0330

    seprate get route

--- ServerCore/API.py
+++ ServerCore/API.py
@@ -13,17 +13,17 @@ def check_api():
     return Response(ApiResponse("OK", "Successfull", "Done"), 200, "application/json")
 
 
-@api.route('/GetPodcasts', methods=["POST", "PUT", "GET"])
-def get_podcasts():
+@api.route('/Podcast', methods=["POST", "PUT", "DELETE"])
+def podcast_operations():
     if request.method == "POST":
         data = request.json
-        if mongo.db.Podcast.find({"id": data["id"]}).count() > 0:
+        if mongo.db.Podcasts.find({"id": data["id"]}).count() > 0:
             return "id alreay exists"
         subsets = []
         for ss in data["subsets"]:
             subsets.append(Subset(ss["id"], ss["name"]).__dict__)
         podcast = Podcast(data["id"], data["name"], data["desc"], data["main"], subsets)
-        res = mongo.db.Podcast.save(podcast.__dict__)
+        res = mongo.db.Podcasts.save(podcast.__dict__)
         return Response(str(res) + " created")
     elif request.method == "PUT":
         data = request.json
@@ -31,8 +31,13 @@ def get_podcasts():
         for ss in data["subsets"]:
             subsets.append(Subset(ss["id"], ss["name"]).__dict__)
         podcast = Podcast(data["id"], data["name"], data["desc"], data["main"], subsets)
-        res = mongo.db.Podcast.update({"id": data["id"]}, podcast.__dict__)
+        res = mongo.db.Podcasts.update({"id": data["id"]}, podcast.__dict__)
         return Response(json.dumps(res), mimetype="application/json")
-    elif request.method == "GET":
-        res = list(mongo.db.Podcast.find({}, {'_id': 0}))
+    elif request.method == "DELETE":
+        return pass
+
+
+@api.route('/Podcasts', methods=["GET"])
+def get_podcasts():
+        res = list(mongo.db.Podcasts.find({}, {'_id': 0}))
         return Response(json.dumps(res), mimetype="application/json")
